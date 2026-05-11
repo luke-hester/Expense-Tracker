354979619025cad11f5ab86ab18939cb56a443e9
luke-hester <lukehester02@gmail.com>
2026-05-11 19:32:56 +0100

    Add csv import/export

--- expense.py
+++ expense.py
@@ -1,26 +1,26 @@
 import datetime
+import csv
+import os
 
 class Expense:
     # Class Attributes
     id = 0
     expenses = []
-
-    def __init__(self, description: str, amount: float):
+    def __init__(self, description: str, amount: float, id: int = None, timestamp: str = None, from_csv: bool = False):
         # Validate arguments
         assert amount >= 0, f"Amount {amount} cannot be negative."
 
-        # Assign arguments to object
+        # Assign values to self
         self.description = description
         self.amount = amount
-
-        # Create values for object
-        self.id = Expense.create_id()
-        self.timestamp = Expense.create_timestamp()
+        self.id = id or Expense.create_id()
+        self.timestamp = timestamp or Expense.create_timestamp()
 
         # Execute actions
         Expense.expenses.append(self)
 
-        print(f"Expense added successfully (ID: {self.id})")
+        if not from_csv:
+            print(f"Expense added successfully (ID: {self.id})")
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.description}', {self.amount}, {self.id}, {self.timestamp})"
@@ -34,3 +34,32 @@ class Expense:
     def create_timestamp():
         dt_obj = datetime.datetime.now()
         return dt_obj.strftime("%Y-%m-%d")
+    
+    @staticmethod
+    def import_from_csv():
+        filename = "expenses.csv"
+        
+        if not os.path.exists(filename):
+            return
+
+        with open(filename, "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                e = Expense(
+                    description=row["description"],
+                    amount=float(row["amount"]),
+                    id=int(row["id"]),
+                    timestamp=row["timestamp"],
+                    from_csv=True
+                    )
+
+        Expense.id = max(e.id for e in Expense.expenses)
+
+    @staticmethod
+    def export_to_csv():
+        fieldnames = ["description", "amount", "id", "timestamp"]
+        with open("expenses.csv", "w", newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            for expense in Expense.expenses:
+                writer.writerow(vars(expense))
--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from cli import parser
 from expense import Expense
 
 def main():
-    args = parser.parse_args()
-    print(args)
+    # Load data
+    Expense.import_from_csv()
 
+    # Get CLI args
+    args = parser.parse_args()
+
+    # Execute commands
     if args.command == "add":
         expense = Expense(args.description, args.amount)
-        print(expense)
+    elif args.command == "list":
+        print("# ID Date Description Amount")
+        for e in Expense.expenses:
+            print(f"{e.id} {e.timestamp} {e.description} {e.amount}")
+    
+    # Save data
+    Expense.export_to_csv()
 
 if __name__ == "__main__":
     main()
