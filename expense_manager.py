ce21a31c79b7bbfa9907310149140ad6607e85cf
luke-hester <lukehester02@gmail.com>
2026-05-12 16:03:51 +0100

    Refactor command logic

---
+++ expense_manager.py
@@ -0,0 +1,84 @@
+import datetime
+from expense import Expense
+
+def add_expense(args):
+    Expense(args.description, args.amount)
+
+def update_expense(args):
+    e = next((x for x in Expense.expenses if x.id == args.id), None)
+    if not e:
+        return f"No expense with id {args.id} found."
+    else:
+        modified = False
+        if args.description:
+            e.description = args.description
+            modified = True
+        if args.amount:
+            e.amount = args.amount
+            modified = True
+
+        if modified:
+            return "Expense updated successfully"
+        else:
+            return "Please pass values to update"
+
+def delete_expense(args):
+    e = next((x for x in Expense.expenses if x.id == args.id), None)
+    if e:
+        Expense.expenses.remove(e)
+        return "Expense deleted successfully"
+    else:
+        return f"No expense with id {args.id} found."
+
+def get_summary(args):
+    if args.month:
+        if 1 <= args.month <= 12:
+            current_year = datetime.datetime.now().year
+
+            total = 0
+            for e in Expense.expenses:
+                e_time_obj = datetime.datetime.strptime(e.timestamp, "%Y-%m-%d")
+                e_year = e_time_obj.year
+                e_month = e_time_obj.month
+                if e_year == current_year and e_month == args.month:
+                    total += e.amount
+            return f"Total expenses for ({current_year}/{args.month}): ${total}"
+        else:
+            return "Invalid month. Please enter value 1 - 12"
+
+    else:
+        total = 0
+        for e in Expense.expenses:
+            total += e.amount
+        return f"Total expenses: ${total}"
+
+def list_expenses(args):
+    if len(Expense.expenses) == 0:
+        return "No expenses saved."
+    
+    lines = ["# ID Date Description Amount"]
+    
+    for e in Expense.expenses:
+        lines.append(f"{e.id} {e.timestamp} {e.description} {e.amount}")
+
+    return "\n".join(lines)
+
+def handle_commands(args):
+    response = ""
+
+    if args.command == "add":
+        response = add_expense(args)
+
+    elif args.command == "list":
+        response = list_expenses(args)
+
+    elif args.command == "delete":
+        response = delete_expense(args)
+
+    elif args.command == "update":
+        response = update_expense(args)
+
+    elif args.command == "summary":
+        response = get_summary(args)
+
+    return response
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
 from cli import parser
 from expense import Expense
+import expense_manager
 
 def main():
     # Load data
@@ -9,65 +9,9 @@ def main():
     # Get CLI args
     args = parser.parse_args()
 
-    # Execute commands
-    if args.command == "add":
-        expense = Expense(args.description, args.amount)
-
-    elif args.command == "list":
-        if len(Expense.expenses) == 0:
-            print("No expenses saved.")
-        else:
-            print("# ID Date Description Amount")
-            for e in Expense.expenses:
-                print(f"{e.id} {e.timestamp} {e.description} {e.amount}")
-
-    elif args.command == "delete":
-        e = next((x for x in Expense.expenses if x.id == args.id), None)
-        if e:
-            Expense.expenses.remove(e)
-            print("Expense deleted successfully")
-        else:
-            print(f"No expense with id {id} found.")
-    
-    elif args.command == "update":
-        e = next((x for x in Expense.expenses if x.id == args.id), None)
-        if not e:
-            print(f"No expense with id {id} found.")
-        else:
-            modified = False
-            if args.description:
-                e.description = args.description
-                modified = True
-            if args.amount:
-                e.amount = args.amount
-                modified = True
-
-            if modified:
-                print("Expense updated successfully")
-            else:
-                print("Please pass values to update")
-
-    elif args.command == "summary":
-        if args.month:
-            if 1 <= args.month <= 12:
-                current_year = datetime.datetime.now().year
-
-                total = 0
-                for e in Expense.expenses:
-                    e_time_obj = datetime.datetime.strptime(e.timestamp, "%Y-%m-%d")
-                    e_year = e_time_obj.year
-                    e_month = e_time_obj.month
-                    if e_year == current_year and e_month == args.month:
-                        total += e.amount
-                print(f"Total expenses for ({current_year}/{args.month}): ${total}")
-            else:
-                print("Invalid month. Please enter value 1 - 12")
-
-        else:
-            total = 0
-            for e in Expense.expenses:
-                total += e.amount
-            print(f"Total expenses: ${total}")
+    # Handle commands
+    response = expense_manager.handle_commands(args)
+    if response: print(response)
     
     # Save data
     Expense.export_to_csv()
